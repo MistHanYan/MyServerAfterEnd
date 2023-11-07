1195689252af20a1a3e5b58d71918518a289eae8
MistHanYan <129029870+MistHanYan@users.noreply.github.com>
2023-11-07 21:10:53 +0800

    Create project

---
+++ src/main/java/com/example/myserverafterend/MyServerAfterEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.myserverafterend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyServerAfterEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyServerAfterEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/myserverafterend/MyServerAfterEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myserverafterend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyServerAfterEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
